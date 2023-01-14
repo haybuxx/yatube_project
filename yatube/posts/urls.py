40d59774dad145410abedbc956a7d483da188f40
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-01-14 22:52:47 +0900

    Создание Templates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,21 +1,12 @@
-# posts/urls.py
 from django.urls import path
-
 from . import views
-
 app_name ='posts'
 
 urlpatterns = [
     #Главная страница
     path('', views.index),
-    # Для страницы, на которой будут посты, отфильтрованные по группам
-    path('posts/', views.group_posts),
-    # Полученные данные запишем в переменную
-    path(
-        'group/<slug:slug>/',
-        views.posts_detail
-    ),
+    #Страница сообществ
+    path('group_list/<slug:slug>/',views.group_list),
+    #Страница для постов
+    path('group_list/<any_dlug>/',views.group_list)
 ]
-#urlpatterns = [
-#    path('ice_cream/', views.ice_cream_list, name='ice_cream_list'),
-#] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Импортируем модель, чтобы обратиться к ней
-from .models import Post
+# Импортируем загрузчик.
+from django.template import loader
+
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
-    # В словаре context отправляем информацию в шаблон
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'posts/index.html', context) 
-
+    # Загружаем шаблон;
+    # шаблоны обычно хранят в отдельной директории.
+    template = loader.get_template('posts/index.html')
+    # Формируем шаблон
+    return HttpResponse(template.render({}, request))
+    #Страница сообществ
 def groups(request, slug):
     return HttpResponse('Страница сообществ')
-
-#def group_posts(request):
-#   return HttpResponse('Посты')
+    #Страница для постов
+def group_list(request):
+    return HttpResponse('Страница для постов')
